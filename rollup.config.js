21648c5b88ee7eaed8672d0c27773c1a4a0157cc
태재영 <tjy970721@gmail.com>
2020-02-02 00:00:35 +0900

    Apply recommend config

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,15 @@
+const fs = require('fs')
 const pkg = require('./package.json');
-const fs = require('fs');
-const rimraf = require('rimraf');
-const {terser} = require('rollup-plugin-terser');
-const {kebabToPascal} = require('naming-convention-transfer');
+const { createConfig, clearDirectory } = require('rollup-config-troy-recommended');
 
-const DIST_DIRECTORY_PATH = './dist';
 const TYPES_FILE_NAME = 'index.d.ts';
-rimraf.sync(DIST_DIRECTORY_PATH);
-fs.mkdirSync(DIST_DIRECTORY_PATH);
-fs.copyFileSync(`./src/${TYPES_FILE_NAME}`, `${DIST_DIRECTORY_PATH}/${TYPES_FILE_NAME}`);
+clearDirectory('./dist');
+fs.copyFileSync(`./src/${TYPES_FILE_NAME}`, `./dist/${TYPES_FILE_NAME}`);
 
-const createConfig = (format) => {
-  const isBrowser = (format === 'iife');
-  const file = (function() {
-    switch (format) {
-      case 'cjs': return pkg.main;
-      case 'esm': return pkg.module;
-      case 'iife': return `dist/${pkg.name}.js`;
-    }
-  })();
-  return {
+module.exports = ['cjs', 'esm', 'iife'].map((format) => (
+  createConfig({
     input: 'src/index.js',
-    output: {
-      banner: `/**\n * @author ${pkg.author.name}\n * @version ${pkg.version}\n * @name ${pkg.name}\n */`,
-      compact: isBrowser,
-      strict : !isBrowser,
-      format: format,
-      file: file,
-      name: kebabToPascal(pkg.name)
-    },
-    plugins: [isBrowser ? terser() : null].filter(Boolean)
-  };
-};
-
-module.exports = [
-  createConfig('cjs'),
-  createConfig('esm'),
-  createConfig('iife')
-];
+    format,
+    package: pkg
+  })
+));
