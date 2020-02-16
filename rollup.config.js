d47fe33fa4c2d0a69eec580c043a67d15639a8e6
태재영 <tjy970721@gmail.com>
2020-02-16 14:01:23 +0900

    remove definition file

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-const fs = require('fs')
 const pkg = require('./package.json');
 const { createConfig, clearDirectory } = require('rollup-config-troy-recommended');
 
-const TYPES_FILE_NAME = 'index.d.ts';
 clearDirectory('./dist');
-fs.copyFileSync(`./src/${TYPES_FILE_NAME}`, `./dist/${TYPES_FILE_NAME}`);
 
 module.exports = ['cjs', 'esm', 'iife'].map((format) => (
   createConfig({
