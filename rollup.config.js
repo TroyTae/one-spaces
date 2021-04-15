44b09ba4e5cf8b1fd70cd0a6856ca98ceeb401a4
태재영 <tjy970721@gmail.com>
2021-04-16 01:56:11 +0900

    fix: change export name

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,27 @@
-const pkg = require('./package.json');
-const fs  = require('fs-extra');
-const {terser} = require('rollup-plugin-terser');
-const {kebabToPascal} = require('naming-convention-transfer');
+const pkg = require("./package.json");
+const fs = require("fs-extra");
+const { terser } = require("rollup-plugin-terser");
 
-const input = 'src/index.js';
+const input = "src/index.js";
 
-function createConfig({ pkg, input, format, file }) {
-  return {	
+function createConfig({ input, format, file }) {
+  return {
     input,
     output: {
       file,
       format,
-      exports: 'auto',
-      name: kebabToPascal(pkg.name)
-    },	
-    plugins: [terser()]
-  };	
+      exports: "auto",
+      name: "spaces",
+    },
+    plugins: [terser()],
+  };
 }
 
-fs.emptyDirSync('./dist');
-fs.copyFileSync('./src/index.d.ts', './dist/index.d.ts');
+fs.emptyDirSync("./dist");
+fs.copyFileSync("./src/index.d.ts", "./dist/index.d.ts");
 
 module.exports = [
-  createConfig({ pkg, input, format: 'cjs', file: pkg.main }),
-  createConfig({ pkg, input, format: 'esm', file: pkg.module }),
-  createConfig({ pkg, input, format: 'iife', file: `dist/${pkg.name}.js` })
+  createConfig({ input, format: "cjs", file: pkg.main }),
+  createConfig({ input, format: "esm", file: pkg.module }),
+  createConfig({ input, format: "iife", file: `dist/${pkg.name}.js` }),
 ];
